09a887192bb9b64f77d03e6f55e4d4220ecdce12
Zane D. R <26946175+TotallyAProgrammer@users.noreply.github.com>
2018-03-23 16:20:14 -0500

    Moved Comments to proper file

--- detect.cs
+++ detect.cs
@@ -16,22 +16,3 @@ class kill
         }
     }
 }
-/*
-Process[] pname = Process.GetProcessesByName("notepad");
- if(pname.Length = 1){
-MessageBox(
-(LPCTSTR)L"nothing"
-);
-}
-else{
-MessageBox(
-(LPCTSTR)L"running"
-);
-}
-if(pname == 1){
-Console.WriteLine("1");
-}
-else{
-Console.WriteLine("0");
-}
-*/
--- kill.cs
+++ kill.cs
@@ -11,3 +11,22 @@ namespace wuakiller{
         }
 }
 }
+/*
+Process[] pname = Process.GetProcessesByName("notepad");
+ if(pname.Length = 1){
+MessageBox(
+(LPCTSTR)L"nothing"
+);
+}
+else{
+MessageBox(
+(LPCTSTR)L"running"
+);
+}
+if(pname == 1){
+Console.WriteLine("1");
+}
+else{
+Console.WriteLine("0");
+}
+*/
