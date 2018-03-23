8c1879e4bddd357c649277d8dd97e5ef6fb9fec5
Bo Universe <kylethespaceengineer@gmail.com>
2018-03-22 23:39:44 -0500

    Rearanged Comments and removed old variable.

--- kill.cs
+++ kill.cs
@@ -7,28 +7,28 @@ class kill
 {
      static void Main()
     {
-//        int a = 1;
         Process[] a = Process.GetProcessesByName("notepad");
         if(a.Length == 1){
             MessageBox.Show("Dot Net Perls");
         }
-/*        if(pname == 1){
-            Console.WriteLine("1");
-        }
-        else{
-            Console.WriteLine("0");
-        }*/
     }
 }
-/*        Process[] pname = Process.GetProcessesByName("notepad");
+/*
+Process[] pname = Process.GetProcessesByName("notepad");
  if(pname.Length = 1){
-            MessageBox(
-                 (LPCTSTR)L"nothing"
-                );
-        }
-        else{
-            MessageBox(
-                 (LPCTSTR)L"running"
-                );
-        }
+MessageBox(
+(LPCTSTR)L"nothing"
+);
+}
+else{
+MessageBox(
+(LPCTSTR)L"running"
+);
+}
+if(pname == 1){
+Console.WriteLine("1");
+}
+else{
+Console.WriteLine("0");
+}
 */
